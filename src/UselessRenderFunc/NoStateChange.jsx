97524b53ab10499102ec8698c62468bddab02cb3
Bian2017 <bgl_cumt_zju@163.com>
2019-11-19 19:37:51 +0800

    feat: 完善场景二的样例

--- src/UselessRenderFunc/NoStateChange.jsx
+++ src/UselessRenderFunc/NoStateChange.jsx
@@ -1,54 +1,35 @@
 import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
 
-let test_cnt = 0
+let test_cnt = 1
 
-class App extends Component {
+class NoStateChange extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      count: 1,
-      testObj: {
-        cnt: 0
-      }
+      count: 1
     }
   }
 
-  componentWillUpdate(nextProps, nextState) {
-    console.log('state:', this.state)
-    console.log('nextState:', nextState)
-    console.log('true:', this.state.testObj === nextState.testObj)
-    console.log('componentWillUpdate')
+  componentWillUpdate() {
     test_cnt = test_cnt + 1
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate')
   }
 
   handleClick = () => {
     this.setState({ count: 1 })
   }
 
-  handleClick1 = () => {
-    let cnt = this.state.testObj
-    cnt.cnt = 1
-    this.setState({ testObj: cnt })
-  }
-
   render() {
     return (
       <div>
-        <h4>每次点击“试验”按钮，数值count会被设置成相同值</h4>
-        <p>数值count: {this.state.count}</p>
-        <button onClick={this.handleClick}>试验</button>
-
-        {this.state.testObj.cnt}
-        <button onClick={this.handleClick1}>Click Me</button>
-        <div>组件发生渲染：{test_cnt}</div>
+        <p>每次点击“试验”按钮，数值count会被设置成相同值。</p>
+        <p>当前count值: {this.state.count}</p>
+        <p>组件发生渲染：{test_cnt}</p>
+        <p>
+          <button onClick={this.handleClick}>试验</button>
+        </p>
       </div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+export default NoStateChange
--- src/UselessRenderFunc/index.jsx
+++ src/UselessRenderFunc/index.jsx
@@ -1,52 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 import ReactDOM from 'react-dom'
 
-let test_cnt = 0
+import NoStateChange from './NoStateChange'
+import OptimizeNoStateChange from './OptimizeNoStateChange'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      count: 1,
-      testObj: {
-        cnt: 0
-      }
-    }
-  }
-
-  componentWillUpdate(nextProps, nextState) {
-    console.log('state:', this.state)
-    console.log('nextState:', nextState)
-    console.log('true:', this.state.testObj === nextState.testObj)
-    console.log('componentWillUpdate')
-    test_cnt = test_cnt + 1
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    console.log('componentDidUpdate')
-  }
-
-  handleClick = () => {
-    this.setState({ count: 1 })
-  }
-
-  handleClick1 = () => {
-    let cnt = this.state.testObj
-    cnt.cnt = 1
-    this.setState({ testObj: cnt })
-  }
-
   render() {
     return (
-      <div>
-        <h4>每次点击“试验”按钮，数值count会被设置成相同值</h4>
-        <p>数值count: {this.state.count}</p>
-        <button onClick={this.handleClick}>试验</button>
-
-        {this.state.testObj.cnt}
-        <button onClick={this.handleClick1}>Click Me</button>
-        <div>组件发生渲染：{test_cnt}</div>
-      </div>
+      <Fragment>
+        <h2>场景：</h2>
+        <strong>给组件的 state 设置相同的值，组件的 state 并未发生变化，但组件依旧发生渲染。</strong>
+        <div>
+          <h4>测试：未性能优化</h4>
+          <NoStateChange />
+        </div>
+        <div>
+          <h4>测试：性能优化后</h4>
+          <OptimizeNoStateChange />
+        </div>
+      </Fragment>
     )
   }
 }
